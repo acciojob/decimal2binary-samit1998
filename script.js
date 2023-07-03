f839ecd243248942ba8c437f62f453ffb1ad9d68
SAMIT KUMAR BARIK <126311037+samit1998@users.noreply.github.com>
2023-07-03 11:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 function decimalToBinary(num) {
   //Write you code here
-	let binary = "";
-  if (num === 0) {
-    return "0";
-  }
-
-  while (num > 0) {
-    binary = (num % 2) + binary;
-    decimal = Math.floor(num / 2);
-  }
-
-  return binary;
+	 return num.toString(2);
 }
+
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
+
 window.decimalToBinary = decimalToBinary;
 
