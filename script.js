038efe5df6756af40f24691b8d66118b18220764
SAMIT KUMAR BARIK <126311037+samit1998@users.noreply.github.com>
2023-07-10 16:31:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function decimalToBinary(num) {
   // Write your code here
+	 return num.toString(2);
 }
+
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
 
 window.decimalToBinary = decimalToBinary;
